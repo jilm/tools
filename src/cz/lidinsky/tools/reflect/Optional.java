f9f9525a98336f49dd19382c9bf4b21e55ea7351
jlidinsky <jlidinsky@gmail.com>
2015-07-14 11:21:33 +0200

    Optional annotation to indicate that some setter or getter is optional.

---
+++ src/cz/lidinsky/tools/reflect/Optional.java
@@ -0,0 +1,27 @@
+package cz.lidinsky.tools.reflect;
+
+/*
+ *  Copyright 2015 Jiri Lidinsky
+ *
+ *  This file is part of java tools library.
+ *
+ *  java tools is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  java tools library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with java tools library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Documented;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Optional { }

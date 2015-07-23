6a70ef5f1900226ec136d6ada5f3d4e9e87352d3
jlidinsky <jlidinsky@gmail.com>
2015-07-23 14:51:58 +0200

    New common exception was added.

---
+++ src/cz/lidinsky/tools/DescriptiveException.java
@@ -0,0 +1,38 @@
+/*
+ *  Copyright 2013, 2014 Jiri Lidinsky
+ *
+ *  This file is part of control4j.
+ *
+ *  control4j is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  control4j is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with control4j.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package cz.lidinsky.tools;
+
+public class DescriptiveException extends BaseException {
+
+  public DescriptiveException() {
+    super();
+  }
+
+  private ExceptionCode code = ExceptionCode.NOT_SPECIFIED;
+
+  public DescriptiveException setCode(ExceptionCode code) {
+    this.code = code;
+    return this;
+  }
+
+  public ExceptionCode getCode() {
+    return code;
+  }
+
+}
---
+++ src/cz/lidinsky/tools/ExceptionCode.java
@@ -0,0 +1,37 @@
+/*
+ *  Copyright 2015 Jiri Lidinsky
+ *
+ *  This file is part of control4j.
+ *
+ *  control4j is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  control4j is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with control4j.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package cz.lidinsky.tools;
+
+public enum ExceptionCode {
+  NOT_SPECIFIED,
+  ILLEGAL_ARGUMENT,
+  NULL_ARGUMENT,
+  NULL_POINTER,
+  BLANK_ARGUMENT,
+  INDEX_OUT_OF_BOUNDS,
+  DUPLICATE_ELEMENT,
+  NO_SUCH_ELEMENT,
+  ILLEGAL_STATE,
+  PARSE,
+  CYCLIC_DEFINITION,
+  CLASS_CAST,
+  CLASS_NOT_FOUND,
+  INSTANTIATION,
+  ILLEGAL_ACCESS
+}
